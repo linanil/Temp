327a5f5cfefece0de236897a185defce294ef7ba
Lina Zhao <lina.lastname@gmail.com>
2012-08-23 12:37:27 +0800

    working version

--- split.pl
+++ split.pl
@@ -11,8 +11,26 @@ my $in_fn="first_9.pdb";
 
 open my $fh, '<', $in_fn;
 
-while(<$fh>){
-	if(/MODEL/../ENDMDL/){
-		print $_;
+my $write_flag;
+my $ofh; 
+
+
+while(my $line = <$fh>){
+	
+	
+	if( $line =~ m{ \A MODEL \s* (\d+) }x ) {
+    		my $model_number = $1;
+    		open $ofh, '>', "$model_number.pdb";
+		$write_flag = 1;
+		}
+	if ( $line =~ /^ENDMDL/ ){
+		print $ofh $line;
+		close $ofh;
+		$write_flag = 0;
+		}
+	
+	if($write_flag){
+		print $ofh $line;
 	}
+	
 }
